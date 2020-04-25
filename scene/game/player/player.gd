357005a477c284d9ee5fc684c48d201500a089fe
gaetan <gaetan.depin@epitech.eu>
2020-04-25 15:16:59 +0200

    add project base

---
+++ scene/game/player/player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+class_name player
+
+export(int) var SPEED = 300
+export(int) var JUMP_HEIGHT = 800
+export(int) var GRAVITY = 40
+
+var vel = Vector2.ZERO
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	update_velocity()
+	vel = move_and_slide(vel, Vector2.UP)
+
+func update_velocity():
+	vel.x = 0
+	if Input.is_action_pressed("movement_left"):
+		vel.x -= 1
+	if Input.is_action_pressed("movement_right"):
+		vel.x += 1
+	if Input.is_action_pressed("movement_jump") and is_on_floor():
+		vel.y -= JUMP_HEIGHT
+	vel.x *= SPEED
+	vel.y += GRAVITY
